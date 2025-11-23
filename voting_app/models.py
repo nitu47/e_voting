d6f9df5e694bd8d720f2534e986a4c41ae83c9f2
Nitu Kumari <nitu.kumari70386@gmail.com>
2025-11-23 10:00:07 +0545

    remove unnecessary code..

--- voting_app/models.py
+++ voting_app/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-'''class Voter(models.Model):
-    name = models.CharField(max_length=150)
-    email = models.EmailField(unique=True)
-    otp = models.CharField(max_length=6, blank=True, null=True)
-    is_verified = models.BooleanField(default=False)
-    has_voted = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.name} <{self.email}>"'''
-
-
 class Voter(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
